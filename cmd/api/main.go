276d4ae3538b34c0b9865a1fd5fab857bab85e21
OGElla <e_dusekenov@kbtu.kz>
2024-05-04 15:44:28 +0500

    origin/main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -45,7 +45,7 @@ func main(){
 	flag.StringVar(&cfg.migrations, "migrations", "", "Path to migration files folder. If not provided, migrations do not applied")
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development | staging | production)")
-	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://postgres:12345@db:5432/healthtracker?sslmode=disable", "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://postgres:12345@localhost:5432/healthtracker?sslmode=disable", "PostgreSQL DSN")
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
